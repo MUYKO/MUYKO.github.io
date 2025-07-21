cd1ee57fc1b06a0c46dd7a43f5b19046611955bd
Boatyn <tomas.boatyn@gmail.com>
2025-07-21 23:13:21 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import { settings } from './src/data/settings';
+import sitemap from "@astrojs/sitemap";
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://MUYKO.github.io',
+  integrations: [sitemap()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/your_instagram_handle/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: 'https://twitter.com/your_twitter_handle',
+  },
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://facebook.com/your_facebook_page',
+  }
+]
