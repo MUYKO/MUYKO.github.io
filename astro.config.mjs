0326f6eab403c9bbcd39c6c6cb4dcdf1fd8f0b2f
Boatyn <tomas.boatyn@gmail.com>
2025-07-21 22:38:36 +0200

    Update site URL for GitHub Pages deployment to Boatyn account

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://treefarmstudio.github.io',
+  site: 'https://Boatyn.github.io',
   base: '/cannonball',
   integrations: [sitemap()],
   vite: {
