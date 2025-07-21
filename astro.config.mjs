77b9c88a20404dae226fec9a2b3658e746f4b3aa
Boatyn <tomas.boatyn@gmail.com>
2025-07-21 22:42:27 +0200

    Update site configuration for MUYKO.github.io organization page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Boatyn.github.io',
-  base: '/cannonball',
+  site: 'https://MUYKO.github.io',
   integrations: [sitemap()],
   vite: {
     ssr: {
